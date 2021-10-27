eb1d3725ed90cb48a85da07f7f9a9a4d581019d7
Josh <joshdaley7@gmail.com>
2021-10-26 23:22:25 -0400

    xor model working

--- test.py
+++ test.py
@@ -26,3 +26,17 @@ print(m)
 
 # each column in the weight matrix is a neuron, the values are its weights
 # eat ass
+
+
+
+layer = tf.keras.layers.Dense(2, activation='relu')
+x = tf.constant([[1., 2., 3.]])
+
+with tf.GradientTape() as tape:
+  # Forward pass
+  y = layer(x)
+  loss = tf.reduce_mean(y**2)
+
+# Calculate gradients with respect to every trainable variable
+grad = tape.gradient(loss, layer.trainable_variables)
+print(layer.trainable_variables)
--- xor_model.py
+++ xor_model.py
@@ -1,51 +1,94 @@
 import tensorflow as tf
-import numpy as np
 
 
 def xor_dataset():
-    return np.array([
-        ( [0, 0], [0] ),
-        ( [0, 1], [1] ),
-        ( [1, 0], [1] ),
-        ( [1, 1], [0] )
-    ], dtype=float)
+    return (
+        (tf.constant([[0.0, 0.0]]), tf.constant([[0.0]])),
+        (tf.constant([[0.0, 1.0]]), tf.constant([[1.0]])),
+        (tf.constant([[1.0, 0.0]]), tf.constant([[1.0]])),
+        (tf.constant([[1.0, 1.0]]), tf.constant([[0.0]]))
+    )
 
 
 class XORModel:
 
     def __init__(self):
         
-        W1 = tf.constant([
-            [0.5, 0.5],
-            [0.5, 0.5]
-        ])
-        B1 = tf.constant([
-            [0.1, 0.2]
-        ])
+        W1 = tf.Variable(
+            tf.random.normal([2, 2], stddev=5)
+        )
+        B1 = tf.Variable(
+            tf.random.normal([1, 2])
+        )
 
-        W2 = tf.constant([
-            [0.5],
-            [0.5]
-        ])
-        B2 = tf.constant([
-            [0.1]
-        ])
+        W2 = tf.Variable(
+            tf.random.normal([2, 1], stddev=5)
+        )
+        B2 = tf.Variable(
+            tf.random.normal([1, 1])
+        )
 
         self.params = [
             (W1, B1),
             (W2, B2)
         ]
 
+        self.params_list = [W1, B1, W2, B2]
+
     def predict(self, input):
-
-        input_mat = tf.constant([input], dtype=float)
         for w, b in self.params:
-            outputs = tf.math.add(tf.matmul(input_mat, w), b)
-            input_mat = outputs
+            output = tf.matmul(input, w) + b
+            output = tf.nn.sigmoid(output)
+            input = output
 
-        print(outputs)
+        return output
+
+    def compute_loss(self, preds, targets):
+        return tf.keras.losses.BinaryCrossentropy()(targets, preds)
+
+    def step_fn(self, batch):
+        with tf.GradientTape() as tape:
+            preds = []
+            targets = []
+            for datum in batch:
+                input, target = datum
+                preds.append(self.predict(input))
+                targets.append(target)
+            loss = self.compute_loss(preds, targets)
+
+        grads = tape.gradient(loss, self.params_list)
+        tf.keras.optimizers.SGD(learning_rate=0.5).apply_gradients(zip(grads, self.params_list))
+        return loss
 
         
 
 mod = XORModel()
-mod.predict([0, 1])
+dataset = xor_dataset()
+
+print('Model:')
+for p in mod.params_list:
+    print(p)
+
+loss = 1
+while loss > 0.05:
+    loss = mod.step_fn(dataset)
+    print(loss)
+
+
+print('Results: ')
+for ex in xor_dataset():
+    inputs, targets = ex
+    print(mod.predict(inputs))
+
+print('Model:')
+for p in mod.params_list:
+    print(p)
+
+
+# mod = XORModel()
+# print('Results: ')
+# for ex in xor_dataset():
+#     inputs, targets = ex
+#     print(mod.predict(inputs))
+
+
