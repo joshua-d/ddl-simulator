4d021446a27f49ccb0f3b9115a7a3577362d9f06
Josh <joshdaley7@gmail.com>
2021-10-27 13:17:47 -0400

    keras model works in dist (from logits), mine does not

---
+++ keras_model.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+import numpy as np
+
+
+def mnist_dataset():
+  (x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
+  x_train = x_train / np.float32(255)
+  y_train = y_train.astype(np.int64)
+  train_dataset = tf.data.Dataset.from_tensor_slices(
+      (x_train, y_train))
+  return train_dataset
+
+
+def build_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='sigmoid'),
+        tf.keras.layers.Dense(10)
+    ])
+
+    return model
