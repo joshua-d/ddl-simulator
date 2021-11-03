a42c2cd1e0e5b91e8546b0c19fd6d74b58c2b85c
Josh <joshdaley7@gmail.com>
2021-11-03 17:02:34 -0400

    2 models, able to average their variables into a new model at the end

--- test.py
+++ test.py
@@ -4,10 +4,25 @@ import keras_model
 # each column in the weight matrix is a neuron, the values are its weights
 
 
-ds = keras_model.mnist_dataset()
-ds = ds.batch(100)
+model1 = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='sigmoid'),
+    tf.keras.layers.Dense(10)
+])
 
-mod = keras_model.build_model()
+model2 = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='sigmoid'),
+    tf.keras.layers.Dense(10)
+])
 
-x, y = next(iter(ds))
-mod.predict(x)
+print(model2.layers[1].kernel.value())
+
+
+v1 = tf.constant([[1, 2, 3],
+                  [4, 5, 6]])
+v2 = tf.constant([[2, 3, 4],
+                  [5, 6, 7]])
+
+r = (v1 + v2) / 2
+print(r)
