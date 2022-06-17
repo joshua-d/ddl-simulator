2d60ae0a526f6b66af5dabe8888b5c8b0906e543
Josh <joshdaley7@gmail.com>
2022-06-16 23:40:15 -0400

    fixed sync - removed waiting nodes - broadcasts to children

--- SyncParameterServer.py
+++ SyncParameterServer.py
@@ -9,11 +9,11 @@ class SyncParameterServer(ParameterServer):
     def __init__(self, id, parents, update_policies, param_locations, ni, params, optimizer, update_interval):
         super().__init__(id, parents, update_policies, param_locations, ni, params, optimizer, update_interval)
 
-        self.round_num_updates_received = 0
+        self.n_worker_updates_this_round = 0
 
 
     def reset_round(self):
-        self.round_num_updates_received = 0
+        self.n_worker_updates_this_round = 0
         
 
     def run(self):
@@ -21,16 +21,13 @@ class SyncParameterServer(ParameterServer):
         while True:
             param_update_buffer = self.ni.ps_wait_for_update(self)
 
-            waiting_nodes = []
-
             # Apply all updates
             with self.params_lock:
                 for param_update in param_update_buffer:
                     param_update.apply(self.params, self.optimizer)
                     if param_update.return_params:
-                        waiting_nodes.append(param_update.sender_id)
+                        self.n_worker_updates_this_round += 1 # TODO this feels hacky - way to detect if update is from child
                     self.n_updates += 1
-                    self.round_num_updates_received += 1
 
                 vals_by_param_id = self.get_params()
 
@@ -40,8 +37,8 @@ class SyncParameterServer(ParameterServer):
                 self.n_updates = 0
                     
             # If all children have sent updates, send params back to waiting nodes
-            if self.round_num_updates_received == len(self.children):
-                self.round_num_updates_received = 0
-                for node_id in waiting_nodes:
+            if self.n_worker_updates_this_round >= len(self.children):
+                self.n_worker_updates_this_round = 0
+                for node_id in self.children:
                     self.ni.send_params_replace(node_id, vals_by_param_id)
 
