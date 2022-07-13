e313c8d1f296071fba1a249efc21616328e6b648
Josh <joshdaley7@gmail.com>
2022-07-13 17:13:42 -0400

    implemented get_available_bw

--- NetworkEmulator.py
+++ NetworkEmulator.py
@@ -7,19 +7,34 @@ TIMING_THREAD_PERIOD = 0.001 # 1ms
 
 
 class Message:
-    def __init__(self, size, dtj_fn, last_checked):
+    def __init__(self, from_id, to_id, size, dtj_fn, last_checked, send_rate):
+        self.from_id = from_id
+        self.to_id = to_id
         self.size = size
         self.dtj_fn = dtj_fn
 
         self.amt_sent = 0
         self.last_checked = last_checked
 
+        self.send_rate = send_rate
+
 
 class NetworkEmulator:
 
-    def __init__(self, bw):
+    def __init__(self, node_bws):
 
-        self.bw = bw
+        # (node_id -> inbound bw, node_id -> outbound bw)
+        self.inbound_max, self.outbound_max = node_bws
+
+        # { node_id -> [msgs] }
+        self.incoming = {}
+        self.outgoing = {}
+
+        for node_id in self.inbound_max.keys():
+            self.incoming[node_id] = []
+            self.outgoing[node_id] = []
+
+        
 
         # Data Transmission Job queue
         self.dtjq = []
@@ -30,20 +45,88 @@ class NetworkEmulator:
         self.sending_msgs_cond = Condition()
 
         # Sending msg timing thread
-        self.timing_thread = Thread(target=self.process_sending_msgs, daemon=True)
+        self.timing_thread = Thread(target=self._process_sending_msgs, daemon=True)
 
         # Data transmission thread
-        self.dt_thread = Thread(target=self.process_dtjq, daemon=True)
+        self.dt_thread = Thread(target=self._process_dtjq, daemon=True)
 
 
-    def send_msg(self, msg_size, dtj_fn):
+    def get_available_bw(self, from_id):
+
+        if len(self.outgoing[from_id]) == 0:
+            return self.outbound_max[from_id]
+
+        # Gather and sort send rates
+        send_rates = []
+
+        for msg in self.outgoing[from_id]:
+            send_rates.append(msg.send_rate)
+
+        send_rates.sort(reverse=True)
+
+        # Calc baseline bw
+        baseline_bw = self.outbound_max[from_id] / len(send_rates)
+
+        # Calc extra bandwidth and how much is in use
+        extra_bw = 0
+        extra_bw_in_use = 0
+
+        for sr in send_rates:
+            if sr < baseline_bw:
+                extra_bw += baseline_bw - sr
+            elif sr > baseline_bw:
+                extra_bw_in_use += sr - baseline_bw
+
+        # Start by giving free bw to avail
+        avail_bw = extra_bw - extra_bw_in_use
+
+        # Check base cases
+        if avail_bw > send_rates[0]:
+            return avail_bw
+
+        # Equalize remaining extra bw
+        sr_idx = 0
+        n_splitting = 1
+
+        while True:
+
+            split_val = (send_rates[sr_idx] - avail_bw) * n_splitting / (n_splitting + 1)
+
+            if sr_idx + 1 == len(send_rates) or avail_bw > send_rates[sr_idx + 1] or avail_bw + split_val > send_rates[sr_idx + 1]:
+                # Ready to split between current srs and avail
+                avail_bw += split_val
+                return avail_bw
+
+            else:
+                # Lower currs, add to avail, add sr_idx + 1 to split
+                avail_bw += (send_rates[sr_idx] - send_rates[sr_idx + 1]) * n_splitting
+                n_splitting += 1
+                sr_idx += 1
+
+
+
+
+
+
+
+
+
+
+    def send_msg(self, from_id, to_id, msg_size, dtj_fn):
         with self.sending_msgs_cond:
+
+            
+
+
             self.sending_msgs.append(Message(msg_size, dtj_fn, time.perf_counter()))
             self.sending_msgs_cond.notify()
 
 
+    
+
+
     # Starting point of timing thread
-    def process_sending_msgs(self):
+    def _process_sending_msgs(self):
         while True:
             sent_msgs = []
 
@@ -51,9 +134,6 @@ class NetworkEmulator:
                 while len(self.sending_msgs) == 0:
                     self.sending_msgs_cond.wait()
 
-                # print(len(self.sending_msgs))
-
-                # TODO do I have to keep it locked here??? I think I do - r/w lock?
                 current_time = time.perf_counter()
                 avail_bw = self.bw / len(self.sending_msgs)
 
@@ -84,7 +164,7 @@ class NetworkEmulator:
 
 
     # Starting point of DT thread
-    def process_dtjq(self):
+    def _process_dtjq(self):
         while True:
 
             with self.dtjq_cond:
@@ -140,3 +220,29 @@ class NetworkEmulator:
                 dtj_fn()
             for msg in msgs:
                 msg.dtj_fn()
+
+
+
+class DM:
+    def __init__(self, send_rate):
+        self.send_rate = send_rate
+
+
+if __name__ == '__main__':
+    inbound = {
+        0: 50,
+        1: 50
+    }
+    outbound = {
+        0: 60,
+        1: 50
+    }
+
+    ne = NetworkEmulator((inbound, outbound))
+
+    ne.outgoing[0].append(DM(10))
+    ne.outgoing[0].append(DM(25))
+    ne.outgoing[0].append(DM(25))
+
+
+    print(ne.get_available_bw(0))
