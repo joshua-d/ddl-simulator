b0384b7fcb57006cc71d3f3c13b4c09b26ac652e
Josh <joshdaley7@gmail.com>
2022-07-18 18:37:34 -0400

    implemented half duplex adding and removing, need to update update_send_rates

--- NetworkEmulator.py
+++ NetworkEmulator.py
@@ -33,6 +33,17 @@ class NetworkEmulator:
             self.incoming[node_id] = []
             self.outgoing[node_id] = []
 
+
+        self.queued_msgs = {}
+        self.active_msgs = {}
+        self.receiving = {} # true if all msgs in active are incoming or active msgs is empty
+
+        for node_id in self.inbound_max.keys():
+            self.queued_msgs[node_id] = []
+            self.active_msgs[node_id] = []
+            self.receiving[node_id] = True
+
+
         self.total_msgs = 0  # TODO could perhaps use len(self.sending_msgs)
 
         # Data Transmission Job queue
@@ -120,15 +131,23 @@ class NetworkEmulator:
     def send_msg(self, from_id, to_id, msg_size, dtj_fn):
         with self.sending_msgs_cond:
             msg = Message(from_id, to_id, msg_size, dtj_fn, time.perf_counter(), 0)
+
+            if len(self.queued_msgs[from_id]) == 0 and (len(self.active_msgs[from_id]) == 0 or not self.receiving[from_id]) and len(self.queued_msgs[to_id]) == 0 and self.receiving[to_id]:
+                self.active_msgs[from_id].append(msg)
+                self.active_msgs[to_id].append(msg)
+                self.total_msgs += 1
+
+                self._update_send_rates()
+
+                self.sending_msgs.append(msg)
+                self.sending_msgs_cond.notify()
+
+            else:
+                self.queued_msgs[from_id].append(msg)
+                self.queued_msgs[to_id].append(msg)
+
+
             
-            self.outgoing[from_id].append(msg)
-            self.incoming[to_id].append(msg)
-            self.total_msgs += 1
-
-            self._update_send_rates()
-
-            self.sending_msgs.append(msg)
-            self.sending_msgs_cond.notify()
 
 
     # Starting point of timing thread
@@ -158,10 +177,86 @@ class NetworkEmulator:
                         msg_idx -= 1
                         sent_msgs.append(msg)
 
-                        # remove from incoming/outgoing and update send rates
-                        self.outgoing[msg.from_id].remove(msg)
-                        self.incoming[msg.to_id].remove(msg)
+                        # remove from active, pull next from queues, update send rates
+                        from_active = self.active_msgs[msg.from_id]
+                        to_active =  self.active_msgs[msg.to_id]
+                        from_queued = self.queued_msgs[msg.from_id]
+                        to_queued = self.queued_msgs[msg.to_id]
+
+                        from_active.remove(msg)
+                        to_active.remove(msg)
                         self.total_msgs -= 1
+
+                        if len(from_active) == 0:
+                            # grab from queue and update mode
+                            if len(from_queued) != 0:
+                                if from_queued[-1].from_id == msg.from_id:
+                                    # outgoing msg
+                                    requeue = []
+                                    while from_queued[-1].from_id == msg.from_id:
+                                        if self.queued_msgs[from_queued[-1].to_id][-1] == from_queued[-1] and self.receiving[from_queued[-1].to_id]:
+                                            from_active.append(from_queued.pop())
+                                            self.active_msgs[from_queued[-1].to_id].append(self.queued_msgs[from_queued[-1].to_id].pop()) 
+                                            self.receiving[msg.from_id] = False
+                                            self.receiving[from_queued[-1].to_id] = True
+                                            self.total_msgs += 1
+                                        else:
+                                            requeue.append(from_queued.pop())
+
+                                    while len(requeue) != 0:
+                                        from_queued.append(requeue.pop())
+
+                                else:
+                                    # incoming msg
+                                    requeue = []
+                                    while from_queued[-1].from_id != msg.from_id:
+                                        if self.queued_msgs[from_queued[-1].from_id][-1] == from_queued[-1] and (not self.receiving[from_queued[-1].from_id] or len(self.active_msgs[from_queued[-1].from_id]) == 0):
+                                            from_active.append(from_queued.pop())
+                                            self.active_msgs[from_queued[-1].from_id].append(self.queued_msgs[from_queued[-1].from_id].pop())
+                                            self.receiving[msg.from_id] = True
+                                            self.receiving[from_queued[-1].to_id] = False
+                                            self.total_msgs += 1
+                                        else:
+                                            requeue.append(from_queued.pop())
+
+                                    while len(requeue) != 0:
+                                        from_queued.append(requeue.pop())
+
+                        if len(to_active) == 0:
+                            # grab from queue and update mode
+                            if len(to_queued) != 0:
+                                if to_queued[-1].from_id == msg.to_id:
+                                    # outgoing msg
+                                    requeue = []
+                                    while to_queued[-1].from_id == msg.to_id:
+                                        if self.queued_msgs[to_queued[-1].to_id][-1] == to_queued[-1] and self.receiving[to_queued[-1].to_id]:
+                                            to_active.append(to_queued.pop())
+                                            self.active_msgs[to_queued[-1].to_id].append(self.queued_msgs[to_queued[-1].to_id].pop()) 
+                                            self.receiving[msg.to_id] = False
+                                            self.receiving[to_queued[-1].to_id] = True
+                                            self.total_msgs += 1
+                                        else:
+                                            requeue.append(to_queued.pop())
+
+                                    while len(requeue) != 0:
+                                        to_queued.append(requeue.pop())
+                                else:
+                                    # incoming msg
+                                    requeue = []
+                                    while to_queued[-1].from_id != msg.to_id:
+                                        if self.queued_msgs[to_queued[-1].from_id][-1] == to_queued[-1] and (not self.receiving[to_queued[-1].from_id] or len(self.active_msgs[to_queued[-1].from_id]) == 0):
+                                            to_active.append(to_queued.pop())
+                                            self.active_msgs[to_queued[-1].from_id].append(self.queued_msgs[to_queued[-1].from_id].pop())
+                                            self.receiving[msg.to_id] = True
+                                            self.receiving[to_queued[-1].to_id] = False
+                                            self.total_msgs += 1
+                                        else:
+                                            requeue.append(to_queued.pop())
+
+                                    while len(requeue) != 0:
+                                        to_queued.append(requeue.pop())
+
+
                         self._update_send_rates()
 
                     msg_idx += 1
