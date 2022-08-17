d665576c93fe629b50e3ab51cda89beedd23c813
Joshua Daley <joshdaley7@gmail.com>
2022-08-17 12:03:04 -0400

    added try except to notify_pushbullet in case connection fails

--- experiments/util/notify_pushbullet.py
+++ experiments/util/notify_pushbullet.py
@@ -3,6 +3,9 @@ from pushbullet import PushBullet
 
 
 def notify_pushbullet(title, text):
-    access_token = 'o.usn8KMIrByNJ9VuRm2aybOsbnvEb4Npv'
-    pb = PushBullet(access_token)
-    pb.push_note(title, text)
+    try:
+        access_token = 'o.usn8KMIrByNJ9VuRm2aybOsbnvEb4Npv'
+        pb = PushBullet(access_token)
+        pb.push_note(title, text)
+    except:
+        print('Failed to notify pushbullet')
