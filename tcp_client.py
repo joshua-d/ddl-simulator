0cf0f244e923a5d9c08b9bf1dcf60108a32d1f6a
Joshua Daley <joshdaley7@gmail.com>
2023-02-10 00:51:10 -0500

    updated client and added data3

--- tcp_client.py
+++ tcp_client.py
@@ -3,18 +3,18 @@ from time import perf_counter
  
 # file is currently 20 Mb
 
-IP = socket.gethostbyname(socket.gethostname()) # "192.168.12.1" # socket.gethostbyname(socket.gethostname())
+IP = '192.168.12.1'
 PORT = 4455
 ADDR = (IP, PORT)
 FORMAT = "utf-8"
-N_SENDS = 200
+N_SENDS = 5
  
 def main():
     # print('reading file')
-    file = open("data.txt", "r")
+    file = open("data3.txt", "r")
     data = file.read().encode(FORMAT)
 
-    print(f'data size: {len(data) / 1_000_000} MB')
+    print(f'data size: {len(data) / 1_000_000} MB, {len(data)} B')
 
     print('connecting to %s' % IP)
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,10 +24,12 @@ def main():
     total_start = perf_counter()
     for i in range(N_SENDS):
         client.send(data)
+        print(f'sent {i}')
     total_end = perf_counter()
 
     print(f'Total time: {total_end - total_start}')
     print(f'Total data sent: {len(data) * N_SENDS / 1_000_000} MB')
+    print(f'Bitrate: {len(data) * N_SENDS * 8 / (total_end - total_start) / 1_000_000} Mbps')
 
     # print('closing file')
     file.close()
