63c1f3790111b067b2c572042bd3d8788d706905
Josh <joshdaley7@gmail.com>
2023-03-23 23:59:01 -0400

    gpts model

--- keras_model.py
+++ keras_model.py
@@ -17,27 +17,21 @@ def test_dataset(num_samples):
 
 # strides and sigmoid activation added according to wikipedia
 def build_model_with_seed(seed):
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(filters=6, kernel_size=(5, 5), input_shape=(32, 32, 3), padding="same", activation="sigmoid",
-            kernel_initializer=initializers.RandomNormal(seed=seed), bias_initializer=initializers.Zeros()),
-
-        tf.keras.layers.AveragePooling2D(strides=(2, 2)),
-
-        tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), activation="sigmoid",
-            kernel_initializer=initializers.RandomNormal(seed=(seed+1)), bias_initializer=initializers.Zeros()),
-
-        tf.keras.layers.AveragePooling2D(strides=(2, 2)),
-
+    # kernel_initializer=initializers.RandomNormal(seed=seed), bias_initializer=initializers.Zeros()
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(32, 32, 3)),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
         tf.keras.layers.Flatten(),
-
-        tf.keras.layers.Dense(120, activation="sigmoid",
-            kernel_initializer=initializers.RandomNormal(seed=(seed+2)), bias_initializer=initializers.Zeros()),
-
-        tf.keras.layers.Dense(84, activation="sigmoid",
-            kernel_initializer=initializers.RandomNormal(seed=(seed+3)), bias_initializer=initializers.Zeros()),
-            
-        tf.keras.layers.Dense(10,
-            kernel_initializer=initializers.RandomNormal(seed=(seed+4)), bias_initializer=initializers.Zeros()) # softmax activation?
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, activation='softmax')
     ])
 
     return model
