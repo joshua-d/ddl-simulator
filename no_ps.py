c8a7650210be87628814a72b14e27d590fd268a9
Josh <joshdaley7@gmail.com>
2023-05-12 13:49:33 -0400

    measurement code in no_ps

--- no_ps.py
+++ no_ps.py
@@ -1,6 +1,7 @@
 from model_and_data_builder import model_builder, dataset_fn
 from DatasetIterator import DatasetIterator
 import keras_model
+from time import perf_counter
 
 
 num_train_samples = 60000
@@ -8,9 +9,6 @@ num_test_samples = 10000
 
 batch_size = 32
 learning_rate = 0.001
-
-eval_interval = 100
-target_acc = 0.95
 
 
 if __name__ == '__main__':
@@ -22,32 +20,57 @@ if __name__ == '__main__':
 
     x_test, y_test = keras_model.test_dataset(num_test_samples)
 
-    print('Beginning training')
-    batch = 1
-    while True:
-        grads_list = forward_pass(next(di))
+    n_batches = 1000
+    fp_time = 0
+    opt_time = 0
+
+    for i in range(n_batches):
+        b = next(di)
+
+        fp_start = perf_counter()
+        grads_list = forward_pass(b)
+        fp_time += perf_counter() - fp_start
+
+        opt_start = perf_counter()
         optimizer.apply_gradients(zip(grads_list, params.values()))
+        opt_time += perf_counter() - opt_start
 
-        if batch % eval_interval == 0:
-            # Evaluate model
-            predictions = model.predict(x_test)            
+    fp_time /= n_batches
+    opt_time /= n_batches
 
-            num_correct = 0
-            for prediction, target in zip(predictions, y_test):
-                answer = 0
-                answer_val = prediction[0]
-                for poss_ans_ind in range(len(prediction)):
-                    if prediction[poss_ans_ind] > answer_val:
-                        answer = poss_ans_ind
-                        answer_val = prediction[poss_ans_ind]
-                if answer == target:
-                    num_correct += 1
+    print(f'fp: {fp_time}')
+    print(f'opt: {opt_time}')
 
-            test_accuracy = float(num_correct) / num_test_samples
-            print(f'Trained {batch} batches')
-            print('Test accuracy: %f' % test_accuracy)
 
-            if test_accuracy >= target_acc:
-                break
+    n_param_sets = [2, 4, 6, 8, 10, 12, 14, 16]
+    n_aggs = 1000
+    for n_param in n_param_sets:
 
-        batch += 1
+        param_sets = []
+
+        for i in range(n_param):
+            _, params, _, _ = model_builder()
+            param_sets.append(params)
+            
+        agg_time = 0
+
+        for i in range(n_aggs):
+            agg_start = perf_counter()
+
+            for param_id in param_sets[0]:
+                param_value = 0
+
+                for param_set in param_sets:
+                    param_value += param_set[param_id]
+                
+                param_value /= len(param_sets)
+
+                param_sets[0][param_id].assign(param_value)
+
+            agg_time += perf_counter() - agg_start
+
+        agg_time /= n_aggs
+        print(f'{n_param} sets, {n_aggs} aggs: {agg_time}')
+
+
+        
