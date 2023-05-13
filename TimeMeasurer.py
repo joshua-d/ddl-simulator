7efdc06719cd365288d7dce9759a6779dfa7eff6
Josh <joshdaley7@gmail.com>
2023-05-12 20:56:07 -0400

    added percent breakdown

--- TimeMeasurer.py
+++ TimeMeasurer.py
@@ -24,5 +24,24 @@ class TimeMeasurer:
         return self.timing
     
     def print(self):
+        total = 0
+
         for key in self.timing:
-            print(f'{key}: {self.timing[key]}')
+            print(f'{key}\t{self.timing[key]}')
+            total += self.timing[key]
+
+        print()
+
+        train_percent = round((self.timing['fp'] + self.timing['opt']) / total * 100, 2)
+        param_percent = round((self.timing['param_assign'] + self.timing['param_aggr']) / total * 100, 2)
+        batch_percent = round((self.timing['batch_fetch']) / total * 100, 2)
+        eval_percent = round((self.timing['eval'] + self.timing['logging']) / total * 100, 2)
+        nsg_percent = round((self.timing['nsg_gen']) / total * 100, 2)
+        other_percent = round((self.timing['other']) / total * 100, 2)
+
+        print(f'Training\t{train_percent}%')
+        print(f'Param aggregation\t{param_percent}%')
+        print(f'Batch fetching\t{batch_percent}%')
+        print(f'Eval/logging\t{eval_percent}%')
+        print(f'First pass generation\t{nsg_percent}%')
+        print(f'Other overhead\t{other_percent}%')
