8ef9f7e0499f4c286864e7dec4de70fe4f313dd9
Joshua Daley <jdaley2@binghamton.edu>
2024-01-21 22:20:51 -0500

    switched to threads and finally got it working

--- madb/vgg16_cifar10.py
+++ madb/vgg16_cifar10.py
@@ -47,11 +47,11 @@ def build_model_with_seed(seed):
 
 
 # In dataset-rework, this just gives the master dataset which is automatically "sharded" by thread-safe DatasetIterator
-def dataset_fn(_):
+def dataset_fn():
     x_train, y_train = train_dataset()
     cifar10_dataset = tf.data.Dataset.from_tensor_slices(
       (x_train, y_train))
-    dataset = cifar10_dataset.shuffle(10000, seed=model_seed, reshuffle_each_iteration=False).batch(32)
+    dataset = cifar10_dataset.shuffle(len(cifar10_dataset), seed=model_seed, reshuffle_each_iteration=False)
     return dataset
 
 
@@ -67,14 +67,14 @@ def model_builder():
 
     train_acc_metric = train_acc_metric_constructor()
 
-    def forward_pass(batch, acc_metric):
+    def forward_pass(batch):
         batch_inputs, batch_targets = batch
         with tf.GradientTape() as tape:
             predictions = model(batch_inputs, training=True)
-            loss = loss_constructor(reduction=tf.keras.losses.Reduction.NONE)(batch_targets, predictions)
+            loss = loss_constructor()(batch_targets, predictions)
 
         grads_list = tape.gradient(loss, model.trainable_variables)
-        acc_metric.update_state(batch_targets, predictions)
+        # acc_metric.update_state(batch_targets, predictions)
         
         return grads_list, loss
 
--- worker_time_demo.py
+++ worker_time_demo.py
@@ -1,110 +1,58 @@
 import multiprocessing
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import random
 import portpicker
 import tensorflow as tf
 import time
 from madb.vgg16_cifar10 import model_builder, dataset_fn
+from threading import Thread
+from DatasetIterator import DatasetIterator
 
 
-def create_in_process_cluster(num_workers, num_ps):
-  """Creates and starts local servers and returns the cluster_resolver."""
-  worker_ports = [portpicker.pick_unused_port() for _ in range(num_workers)]
-  ps_ports = [portpicker.pick_unused_port() for _ in range(num_ps)]
+N_WORKER = 8
+N_STEPS = 5
 
-  cluster_dict = {}
-  cluster_dict["worker"] = ["localhost:%s" % port for port in worker_ports]
-  if num_ps > 0:
-    cluster_dict["ps"] = ["localhost:%s" % port for port in ps_ports]
+def main():
 
-  cluster_spec = tf.train.ClusterSpec(cluster_dict)
+  threads = []
+  mean_step_times = []
 
-  # Workers need some inter_ops threads to work properly.
-  worker_config = tf.compat.v1.ConfigProto()
-  if multiprocessing.cpu_count() < num_workers + 1:
-    worker_config.inter_op_parallelism_threads = num_workers + 1
+  dataset = dataset_fn()
+  di = DatasetIterator(dataset, 32)
 
-  for i in range(num_workers):
-    tf.distribute.Server(
-        cluster_spec,
-        job_name="worker",
-        task_index=i,
-        config=worker_config,
-        protocol="grpc")
+  for i in range(N_WORKER):
+    model, _, forward_pass, build_optimizer, _, _, bs, lr = model_builder()
+    optimizer = build_optimizer(lr)
+    mean_step_times.append(0)
 
-  for i in range(num_ps):
-    tf.distribute.Server(
-        cluster_spec,
-        job_name="ps",
-        task_index=i,
-        protocol="grpc")
+    my_batches = []
+    for _ in range(N_STEPS):
+      my_batches.append(next(di))
 
-  cluster_resolver = tf.distribute.cluster_resolver.SimpleClusterResolver(
-      cluster_spec, rpc_layer="grpc")
-  return cluster_resolver
+    def work(idx):
+      sum_step_time = 0
+      for j in range(N_STEPS):
+        start_time = time.perf_counter()
+        gradients, loss = forward_pass(my_batches[j])
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        end_time = time.perf_counter()
+        sum_step_time += end_time - start_time
 
-# Set the environment variable to allow reporting worker and ps failure to the
-# coordinator. This is a workaround and won't be necessary in the future.
-os.environ["GRPC_FAIL_FAST"] = "use_caller"
+      mean_step_time = sum_step_time / N_STEPS
+      mean_step_times[idx] = mean_step_time
+  
+    t = Thread(target=work, args=(i,))
+    threads.append(t)
 
-NUM_WORKERS = 10
-NUM_PS = 1
-cluster_resolver = create_in_process_cluster(NUM_WORKERS, NUM_PS)
+  for t in threads:
+    t.start()
 
+  for t in threads:
+    t.join()
 
-variable_partitioner = (
-    tf.distribute.experimental.partitioners.MinSizePartitioner(
-        min_shard_bytes=(256 << 10),
-        max_shards=NUM_PS))
-
-strategy = tf.distribute.experimental.ParameterServerStrategy(
-    cluster_resolver,
-    variable_partitioner=variable_partitioner)
+  print(mean_step_times)
 
 
-
-with strategy.scope():
-  model, _, forward_pass, build_optimizer, _, accuracy, bs, lr = model_builder()
-  optimizer = build_optimizer(lr)
-
-
-
-@tf.function
-def step_fn(iterator):
-
-  def replica_fn(batch):
-    start_time = time.perf_counter()
-    forward_pass(batch, accuracy)
-    end_time = time.perf_counter()
-    return end_time - start_time
-
-  batch = next(iterator)
-  step_time = strategy.run(replica_fn, args=(batch,))
-  return step_time
-
-
-coordinator = tf.distribute.experimental.coordinator.ClusterCoordinator(strategy)
-
-
-@tf.function
-def per_worker_dataset_fn():
-  return strategy.distribute_datasets_from_function(dataset_fn)
-
-per_worker_dataset = coordinator.create_per_worker_dataset(per_worker_dataset_fn)
-per_worker_iterator = iter(per_worker_dataset)
-
-
-num_epochs = 4
-steps_per_epoch = 5
-times = []
-for i in range(num_epochs):
-  accuracy.reset_states()
-  for _ in range(steps_per_epoch):
-    step_time = coordinator.schedule(step_fn, args=(per_worker_iterator,))
-    times.append(step_time)
-  # Wait at epoch boundaries.
-  coordinator.join()
-  print("Finished epoch %d, accuracy is %f." % (i, accuracy.result().numpy()))
-
-for t in times:
-  print(t.fetch())
+if __name__ == '__main__':
+  main()
