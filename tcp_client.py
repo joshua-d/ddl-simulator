257010be685679fee59bb929389341cf8ecd176f
Joshua Daley <joshdaley7@gmail.com>
2024-01-25 17:23:35 -0500

    tcp client changes

--- tcp_client.py
+++ tcp_client.py
@@ -1,18 +1,20 @@
 import socket
 from time import perf_counter
+import random
  
 # file is currently 20 Mb
 
-IP = '192.168.12.1'
+IP = '10.42.0.47'
 PORT = 4455
 ADDR = (IP, PORT)
-FORMAT = "utf-8"
-N_SENDS = 5
+FORMAT = "ascii"
+
+DATA_SIZE = 1_000_000
+
  
 def main():
-    # print('reading file')
-    file = open("data3.txt", "r")
-    data = file.read().encode(FORMAT)
+    print('generating data')
+    data = random.randbytes(DATA_SIZE)
 
     print(f'data size: {len(data) / 1_000_000} MB, {len(data)} B')
 
@@ -22,17 +24,13 @@ def main():
  
     print('sending')
     total_start = perf_counter()
-    for i in range(N_SENDS):
-        client.send(data)
-        print(f'sent {i}')
+    client.sendall(data)
+    print('sent all')
     total_end = perf_counter()
 
     print(f'Total time: {total_end - total_start}')
-    print(f'Total data sent: {len(data) * N_SENDS / 1_000_000} MB')
-    print(f'Bitrate: {len(data) * N_SENDS * 8 / (total_end - total_start) / 1_000_000} Mbps')
-
-    # print('closing file')
-    file.close()
+    print(f'Total data sent: {len(data) / 1_000_000} MB')
+    print(f'Bitrate: {len(data) * 8 / (total_end - total_start) / 1_000_000} Mbps')
 
     # print('closing client')
     client.close()
