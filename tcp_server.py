bc2dd869c3a8e86013ccf38683c48ee2aefc7e37
Joshua Daley <joshdaley7@gmail.com>
2024-01-25 17:24:35 -0500

    tcp server changes

--- tcp_server.py
+++ tcp_server.py
@@ -1,11 +1,12 @@
 import socket
 from time import perf_counter, sleep
  
-IP = '192.168.12.1' # socket.gethostbyname(socket.gethostname())
+IP = '10.42.0.47' # socket.gethostbyname(socket.gethostname())
 PORT = 4455
 ADDR = (IP, PORT)
-SIZE = 5_000_000
-FORMAT = "utf-8"
+FORMAT = "ascii"
+
+DATA_SIZE = 1_000_000
  
 def main():
     print("[STARTING] Server is starting.")
@@ -18,8 +19,19 @@ def main():
         conn, addr = server.accept()
         print(f"[NEW CONNECTION] {addr} connected.")
 
+        bytes_received = 0
+        start_time = perf_counter()
         while True:
-            data = conn.recv(4096).decode(FORMAT)
+            data = conn.recv(4096)
+            bytes_received += len(data)
+            if bytes_received == DATA_SIZE:
+                end_time = perf_counter()
+                break
+
+        print('received all')
+        print(f'Total time: {end_time - start_time}')
+        print(f'Bitrate: {DATA_SIZE * 8 / (end_time - start_time) / 1_000_000} Mbps')
+
 
  
 if __name__ == "__main__":
